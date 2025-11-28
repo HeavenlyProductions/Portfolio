9d59b11b062ee9e3e85de9a8c6835712cdb63238
MR YARTEY <emmanueljrplays@gmail.com>
2025-11-28 08:03:02 +0000

    Contact form complete

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,8 +1,11 @@
+'use client'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
     return (
-        <div className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("../assets/public/footer-bg-color.png")]
+        <div className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")]
          bg-no-repeat bg-cover bg-[legnth: 90%_auto]'>
             <h4 className='text-center mb-2 text-lg font-ovo'>Connect with me</h4>
             <h2 className='text-center text-5xl font-ovo'>Get in touch</h2>
@@ -12,14 +15,27 @@ const Contact = () => {
             </p>
 
             <form>
-                <div>
-                    <input type="text" placeholder='Enter your name' required />
-                    <input type="email" placeholder='Enter your email' required />
+                <div className='grid auto gap-6 mt-10 mb-8'>
+                    <input type="text" placeholder='Enter your name' required
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 
+                        rounded-md bg-white' />
+                    <input type="email" placeholder='Enter your email' required
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 
+                        rounded-md bg-white' />
                 </div>
 
-                <textarea rows='6' placeholder='Enter your message' required></textarea>
+                <textarea rows='6' placeholder='Enter your message' required
+                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 
+                    rounded-md bg-white mb-6'></textarea>
 
-                <button type='submit'>Submit now</button>
+                <button type='submit'
+                    className='py-3 px-8 w-max flex items-center justify-between gap-2
+                  bg-black/80 text-white rounded-full mx-auto hover:bg-black 
+                    duration-300'>
+                    Submit now <Image src={assets.right_arrow_white} alt='' className='w-4' />
+                </button>
+
+                <p className='mt-4'>sending....</p>
 
             </form>
 
