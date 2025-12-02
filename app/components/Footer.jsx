99be5b333bd5fdfe472a878ad5c201597b6d6e81
MR YARTEY <emmanueljrplays@gmail.com>
2025-12-02 00:03:56 +0000

    Footer added

---
+++ app/components/Footer.jsx
@@ -0,0 +1,31 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='mt-20'>
+
+            <div className='text-center'>
+                <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2' />
+            </div>
+
+            <div className='w-max flex items-center gap-2 mx-auto'>
+                <Image src={assets.mail_icon} alt='' className='w-6' /> emmanueljrplays@gmail.com
+            </div>
+
+            <div className='text-center sm:flex items-center justify-between border-t
+               border-gray-400 mx-[10%] mt-12 py-6'>
+                <p>@ 2025 Emmanuel Yartey. All rights reserved.</p>
+                <ul className='flex items-center justify-center gap-10 mt-4 sm:mt-0'>
+                    <li><a target='_blank' href="https://instagram.com/solace">Github</a></li>
+                    <li><a target='_blank' href="https://instagram.com/solace">LinkedIn</a></li>
+                    <li><a target='_blank' href="https://instagram.com/solace">Twitter</a></li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
